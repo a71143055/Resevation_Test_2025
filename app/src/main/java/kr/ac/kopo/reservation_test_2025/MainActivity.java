17630ad1643c8a95be51c2c121eb43c0fcfe5325
a71143055 <a71143055@gmail.com>
2025-05-12 16:12:49 +0900

    예약

--- app/src/main/java/kr/ac/kopo/reservation_test_2025/MainActivity.java
+++ app/src/main/java/kr/ac/kopo/reservation_test_2025/MainActivity.java
@@ -91,6 +91,7 @@ public class MainActivity extends AppCompatActivity {
             selectedDay = date_pick.getDayOfMonth();
             selectedHour = time_pick.getHour();
             selectedMinute = time_pick.getMinute();
+            rg.setVisibility(View.INVISIBLE);
             date_pick.setVisibility(View.INVISIBLE);
             time_pick.setVisibility(View.INVISIBLE);
             text_result.setText("예약 완료 : " + selectedYear + "년 " + selectedMonth + "월 " + selectedDay + "일 " + selectedHour + "시 " + selectedMinute + "분");
