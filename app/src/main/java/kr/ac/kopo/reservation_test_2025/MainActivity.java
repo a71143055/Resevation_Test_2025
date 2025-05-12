848d8eea0e7d70481c0cf494a8b158592c0e748e
a71143055 <a71143055@gmail.com>
2025-05-12 16:05:18 +0900

    예약

--- app/src/main/java/kr/ac/kopo/reservation_test_2025/MainActivity.java
+++ app/src/main/java/kr/ac/kopo/reservation_test_2025/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.Chronometer;
+import android.widget.DatePicker;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -23,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     Chronometer chronometer;
     RadioGroup rg;
     RadioButton rb_date, rb_time;
-    CalendarView calendar;
+    DatePicker date_pick;
     TimePicker time_pick;
     TextView text_result;
     Button btn_start, btnDone;
@@ -43,59 +44,55 @@ public class MainActivity extends AppCompatActivity {
         });
 
         chronometer = findViewById(R.id.chronometer);
-        btn_start = findViewById(R.id.btn_start);
         rg = findViewById(R.id.rg);
         rb_date = findViewById(R.id.rb_date);
         rb_time = findViewById(R.id.rb_time);
-        calendar = findViewById(R.id.calendar);
+        date_pick = findViewById(R.id.date_pick);
         time_pick = findViewById(R.id.time_pick);
         text_result = findViewById(R.id.text_result);
-        btnDone = findViewById(R.id.btn_done);
 
-        calendar.setVisibility(View.INVISIBLE);
+        date_pick.setVisibility(View.INVISIBLE);
         time_pick.setVisibility(View.INVISIBLE);
         rb_date.setOnClickListener(rbListener);
         rb_time.setOnClickListener(rbListener);
-        btn_start.setOnClickListener(btnListener);
-        btnDone.setOnClickListener(btnListener);
-        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                selectedYear = year;
-                selectedMonth = month + 1;
-                selectedDay = dayOfMonth;
-            }
-        });
+        chronometer.setOnClickListener(chronoListener);
+        text_result.setOnLongClickListener(textListener);
     }
     View.OnClickListener rbListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             RadioButton rbEvent = (RadioButton) v;
             if(rbEvent == rb_date) {
-                calendar.setVisibility(View.VISIBLE);
+                date_pick.setVisibility(View.VISIBLE);
                 time_pick.setVisibility(View.INVISIBLE);
             } else if(rbEvent == rb_time) {
-                calendar.setVisibility(View.INVISIBLE);
+                date_pick.setVisibility(View.INVISIBLE);
                 time_pick.setVisibility(View.VISIBLE);
             }
         }
     };
 
-    View.OnClickListener btnListener = new View.OnClickListener() {
+    View.OnClickListener chronoListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Button btnEvent = (Button) v;
-            if(btnEvent == btn_start) {
-                chronometer.setBase(SystemClock.elapsedRealtime());
-                chronometer.start();
-                chronometer.setTextColor(Color.RED);
-            } else if(btnEvent == btnDone) {
-                chronometer.stop();
-                chronometer.setTextColor(Color.BLUE);
-                selectedHour = time_pick.getHour();
-                selectedMinute = time_pick.getMinute();
-                text_result.setText("예약 완료 : " + selectedYear + "년 " + selectedMonth + "월 " + selectedDay + "일 " + selectedHour + "시 " + selectedMinute + "분");
-            }
+            chronometer.setBase(SystemClock.elapsedRealtime());
+            chronometer.start();
+            chronometer.setTextColor(Color.RED);
+        }
+    };
+
+    View.OnLongClickListener textListener = new View.OnLongClickListener() {
+        @Override
+        public boolean onLongClick(View v) {
+            chronometer.stop();
+            chronometer.setTextColor(Color.BLUE);
+            selectedYear = date_pick.getYear();
+            selectedMonth = date_pick.getMonth() + 1;
+            selectedDay = date_pick.getDayOfMonth();
+            selectedHour = time_pick.getHour();
+            selectedMinute = time_pick.getMinute();
+            text_result.setText("예약 완료 : " + selectedYear + "년 " + selectedMonth + "월 " + selectedDay + "일 " + selectedHour + "시 " + selectedMinute + "분");
+            return true;
         }
     };
 }
